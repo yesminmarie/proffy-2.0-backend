d41cdab11fd52b6b46b48e8c20f14023422df560
yesmin <ymslahoud@gmail.com>
2020-10-08 21:31:06 -0300

    migration CreateSchedules

---
+++ src/database/migrations/1602201000088-CreateSchedules.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateSchedules1602201000088
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+        await queryRunner.createTable(
+            new Table({
+                name: 'schedules',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+
+                    {
+                        name: 'week_day',
+                        type: 'integer',
+                    },
+
+                    {
+                        name: 'from',
+                        type: 'integer',
+                    },
+
+                    {
+                        name: 'to',
+                        type: 'integer',
+                    },
+
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('schedules');
+    }
+}
