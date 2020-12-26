5ad14e148564eaaccdcc68ef8c4b7483e1087efe
yesmin <ymslahoud@gmail.com>
2020-12-26 09:35:13 -0300

    cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from '@shared/infra/http/routes';
@@ -11,8 +12,9 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
